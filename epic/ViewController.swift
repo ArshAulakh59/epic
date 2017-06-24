2451b30ee34c0897cc9892aa199d1d5bcc6cd1d0
ArshAulakh <arshaulakh@thebhouse.com>
2017-06-24 11:00:29 +0530

    Initial Commit

---
+++ epic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  epic
+//
+//  Created by TWG on 2017-06-24.
+//  Copyright © 2017 AA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
